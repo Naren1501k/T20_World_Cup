e505ee933acfeb1e73e222419a8ff0408c8f7afe
DT_Narendirank <your-email@example.com>
2024-07-05 13:15:26 +0530

    adding some functions in cricket.rb

--- script/cricket.rb
+++ script/cricket.rb
@@ -19,31 +19,36 @@ module Cricket
     end
 
     def MAX_players
-      #
+      if @number > 11
+        @limit = true
+        end
     end
 
     def Add_players
-        #
+        if @limit == true
+            puts "Team is full"
+        else
+            puts "Enter the name of the player"
+            name = gets.chomp
+            @number += 1
+            puts "Player added"
+        end    
     end
 
     def Remove_players
-        #
-    end
+        if @number == 0
+            puts "Team is empty"
+        else
+            puts "Enter the name of the player"
+            name = gets.chomp
+            @number -= 1
+            puts "Player removed"
+        end
 
-    def print_statistics
-        #
+    def Team_statistics
+        puts "Team: #{@team}"
+        puts "Number of players #{@number}"
     end
-
-  
-    def limit
-      raise NotImplementedError, "Must be implemented in the subclass"
-    end
-  
-    def return
-      raise NotImplementedError, "Must be implemented in the subclass"
-    end
-  
-  end
   
 class Player < Team
     include Team
@@ -57,19 +62,23 @@ class Player < Team
     end
 
     def Add_runs
-        #
-      end
+        puts "Enter the runs scored by the player"
+        runs = gets.chomp.to_i
+        @player_details[:runs] += runs
+    end
     
     def Add_wickets
-        #
+        puts "Enter the wickets taken by the player"
+        wickets = gets.chomp.to_i
+        @player_details[:wickets] += wickets
+
     end
     
     def Player_statistics
-        #
-    end
+        puts "Player: #{@player_details[:name]}"
+        puts "Runs: #{@player_details[:runs]}"
+        puts "Wickets: #{@player_details[:wickets]}"
 
-    def display_playerdetails
-       #
     end
     
 end
